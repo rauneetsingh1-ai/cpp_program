d206f7044fb9f94eb107e4a3bf46886b789a7c4b
Rauneet Singh <rauneetsingh1@gmail.com>
2026-06-17 15:02:56 +0530

    Add files via upload

---
+++ area_of_sq.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the side of square: ";
+    cin>>n;
+    int area=n*n;
+    cout<<"area of square is: "<<area;
+    return 0;
+}
