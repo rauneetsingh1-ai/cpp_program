d65e9f7cf49bbec08d0526ae2640311abf550951
Rauneet Singh <rauneetsingh1@gmail.com>
2026-06-17 15:02:10 +0530

    Add files via upload

---
+++ while_loop.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number:";
+    cin>>n;
+    int sum=0;
+    while(n>0){
+        int lastdig=n%10;
+        sum+=lastdig;
+        n=n/10;
+    }
+    cout<<"sum of digits is: "<<sum;
+    cout<<endl;
+    return 0;
+}
